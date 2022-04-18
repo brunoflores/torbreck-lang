dec160ea60993f9913693e2acfc72c3ad9c34463
Bruno Flores <bruno@brunoflores.com.br>
2022-04-18 10:34:39 +1000

    Format.

--- lib/support.mli
+++ lib/support.mli
@@ -2,9 +2,7 @@ module Error : sig
   type info
 
   val dummyinfo : info
-
   val createInfo : string -> int -> int -> int -> info
-
   val printInfo : info -> unit
 
   type 'a withinfo = { i : info; v : 'a }
@@ -14,17 +12,14 @@ module Error : sig
      calls to Format.print_space to print a space or, if necessary,
      break the line at that point. *)
   val errf : (unit -> unit) -> 'a
-
   val errfAt : info -> (unit -> unit) -> 'a
 
   (* Convenient wrappers for the above, for the common case where the
      action to be performed is just to print a given string. *)
   val err : string -> 'a
-
   val error : info -> string -> 'a
 
   (* Variants that print a message but do not fail afterwards *)
   val warning : string -> unit
-
   val warningAt : info -> string -> unit
 end
