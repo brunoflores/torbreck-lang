c0dee3f4477b7f602bdce83f17d7ef8e242eb932
Bruno Flores <bruno@brunoflores.com.br>
2022-04-24 10:51:19 +1000

    Test object.

--- test/test_15.f
+++ test/test_15.f
@@ -1,2 +1,8 @@
 Counter = {get: Unit -> Nat, inc: Unit -> Unit};
 inc3 = lambda x: Counter. (x.inc unit; x.inc unit; x.inc unit);
+
+c = let x = ref 1 in
+      { get = (lambda _:Unit. !x),
+        inc = (lambda _:Unit. x := succ (!x)) };
+
+(inc3 c; c.get unit);
