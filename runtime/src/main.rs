b82be36a9b4ae5fdef6e7450f3813508e4bf77ca
Bruno Flores <bruno@brunoflores.com.br>
2022-05-11 08:35:10 +1000

    Add working cli.

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn usage() -> &'static str {
+    "Usage: breckrun filename"
+}
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() != 2 {
+            return Err(usage());
+        }
+
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|usage| {
+        println!("{}", usage);
+        process::exit(1);
+    });
+    println!("filename: {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).unwrap_or_else(|_| {
+        println!("could not read file");
+        process::exit(1);
+    });
+    println!("with text:\n{}", contents);
 }
