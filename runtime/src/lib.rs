c14f5349e579848d4c54722cbb77a5ad8b9accd9
Bruno Flores <bruno@brunoflores.com.br>
2022-05-15 11:27:53 +1000

    Implement zam.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -2,6 +2,9 @@ use std::error::Error;
 use std::fmt;
 use std::fs;
 
+mod opcodes;
+use opcodes::Instruction;
+
 #[derive(Debug)]
 pub struct ConfigError;
 
@@ -28,9 +31,52 @@ impl Config {
     }
 }
 
+struct Machine {
+    mem: Vec<u8>,
+    pc: usize,
+    asp: u8,
+    rsp: u8,
+    accu: u8,
+}
+
+impl Machine {
+    pub fn new(mem: Vec<u8>) -> Self {
+        Machine {
+            mem,
+            pc: 0,
+            accu: 0,
+            asp: 0,
+            rsp: 0,
+        }
+    }
+
+    pub fn interpret(&mut self) {
+        loop {
+            match self.decode() {
+                Instruction::Stop => break,
+                _ => panic!("not implemented"),
+            }
+        }
+    }
+
+    pub fn accu(&self) -> u8 {
+        self.accu
+    }
+
+    fn decode(&self) -> Instruction {
+        opcodes::decode(self.mem[self.pc])
+    }
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("with text:\n{}", contents);
+    let contents = fs::read(config.filename)?;
+
+    // println!("with bytes:\n{:?}", contents);
+    // println!("first instruction: {:?}", opcodes::decode(contents[0]));
+
+    let mut machine = Machine::new(contents);
+    machine.interpret();
+    println!("accumulator is: {}", machine.accu());
     Ok(())
 }
 
