11b8d4bb5b533ede2355da730bd65615bab3b0e5
Bruno Flores <bruno@brunoflores.com.br>
2022-05-29 11:35:02 +1000

    Begin parsing external primitive declarations.

--- light/lib/ty_decl.ml
+++ light/lib/ty_decl.ml
@@ -2,6 +2,8 @@
 
 open Types
 open Typing
+open Modules
+open Globals
 
 let type_expression _loc expr =
   push_type_level ();
@@ -9,3 +11,14 @@ let type_expression _loc expr =
   pop_type_level ();
   if is_nonexpansive expr then generalize_type ty;
   ty
+
+let type_valuedecl _loc decl =
+  let enter_val (name, typexp, prim) =
+    push_type_level ();
+    reset_type_expression_vars ();
+    let ty = type_of_type_expression false typexp in
+    pop_type_level ();
+    generalize_type ty;
+    add_value (defined_global name { val_typ = ty; val_prim = prim })
+  in
+  List.iter enter_val decl
