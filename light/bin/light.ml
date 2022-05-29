d4a652c37ed8863512b2f9babb32b9c5eed6ccdc
Bruno Flores <bruno@brunoflores.com.br>
2022-05-29 11:36:07 +1000

    Begin parsing external primitive declarations.

--- light/bin/light.ml
+++ light/bin/light.ml
@@ -1,6 +1,7 @@
 (* Command-line parsing *)
 
 open LightLib.Compiler
+open LightLib.Modules
 
 let () =
   let usage = "light [-d] <file1> [<file2>] ..." in
@@ -13,12 +14,14 @@ let () =
   Arg.parse spec readfname usage;
   match !filename with
   | Some f ->
-      if Filename.check_suffix f ".ml" then begin
+      default_used_modules := [ "builtin" ];
+      if Filename.check_suffix f ".ml" then
         let filename = Filename.chop_suffix f ".ml" in
-        compile_implementation filename ".ml";
-        exit 0
-      end
-      else exit 1
+        compile_implementation (Filename.basename filename) filename ".ml"
+      else if Filename.check_suffix f ".mli" then
+        let filename = Filename.chop_suffix f ".mli" in
+        compile_interface (Filename.basename filename) filename
+      else failwith @@ Printf.sprintf "Don't know what to do with file %s." f
   | None ->
       print_endline usage;
       exit 0
