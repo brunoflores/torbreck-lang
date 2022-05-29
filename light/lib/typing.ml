3619242bbd78be0a388d8e85230feabac99bd20f
Bruno Flores <bruno@brunoflores.com.br>
2022-05-29 11:35:39 +1000

    Begin parsing external primitive declarations.

--- light/lib/typing.ml
+++ light/lib/typing.ml
@@ -3,6 +3,44 @@
 open Syntax
 open Const
 open Builtins
+open Globals
+open Error
+open Types
+open Modules
+
+(* To convert type expressions to types *)
+let type_expr_vars = ref ([] : (string * typ) list)
+let reset_type_expression_vars () = type_expr_vars := []
+
+let type_of_type_expression strict_flag typexp =
+  let rec type_of typexp =
+    match typexp.te_desc with
+    | Ztypevar v -> begin
+        try List.assoc v !type_expr_vars
+        with Not_found ->
+          if strict_flag then unbound_type_var_err v typexp
+          else begin
+            let t = new_global_type_var () in
+            type_expr_vars := (v, t) :: !type_expr_vars;
+            t
+          end
+      end
+    | Ztypearrow (arg1, arg2) -> type_arrow (type_of arg1, type_of arg2)
+    | Ztypeconstr (cstr_name, args) ->
+        let cstr =
+          try find_type_desc cstr_name
+          with Desc_not_found ->
+            unbound_type_constr_err cstr_name typexp.te_loc
+        in
+        if List.length args != cstr.info.ty_arity then
+          type_arity_err cstr args typexp.te_loc
+        else
+          {
+            typ_desc = Tconstr (cstr.info.ty_constr, List.map type_of args);
+            typ_level = notgeneric;
+          }
+  in
+  type_of typexp
 
 (* Check if an expression is non-expansive, that is, the result of its
    evaluation cannot contain newly created mutable objects. *)
