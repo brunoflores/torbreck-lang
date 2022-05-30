67afbe0b48fc5d9172a14765db93f8127fe65e2b
Bruno Flores <bruno@brunoflores.com.br>
2022-05-31 07:09:01 +1000

    Remove primitive.

--- light/lib/primdecl.ml
+++ light/lib/primdecl.ml
@@ -3,7 +3,7 @@
 open Prim
 open Globals
 
-let primitive_names = [ ("print_string", Pprintstring) ]
+let primitive_names = []
 
 let find_primitive arity name =
   try ValuePrim (arity, List.assoc name primitive_names)
