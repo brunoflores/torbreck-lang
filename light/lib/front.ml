a74571f71cdbd105bf2c6c25707870df4540d546
Bruno Flores <bruno@brunoflores.com.br>
2022-05-30 19:53:15 +1000

    Organise.

--- light/lib/front.ml
+++ light/lib/front.ml
@@ -12,9 +12,8 @@ let translate_expr env =
     | Zident { contents = Zlocal s } -> translate_access s env
     | Zconstant c -> Lconst c
     | x ->
-        failwith
-        @@ Printf.sprintf "not implemented: Front.translate_expr: %s"
-             (Syntax.show_expression_desc x)
+        Printf.printf "%s" (Syntax.show_expression_desc x);
+        failwith "not implemented: Front.translate_expr"
   in
   transl
 
