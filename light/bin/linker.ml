8632e3a1dc080b634e5cb15d022d3994f15359da
Bruno Flores <bruno@brunoflores.com.br>
2022-05-31 18:41:54 +1000

    Begin linker.

--- light/bin/linker.ml
+++ light/bin/linker.ml
@@ -5,7 +5,7 @@ let () =
   let debug = ref false in
   let files = ref ([] : string list) in
   let spec = [ ("-d", Arg.Set debug, "Print debug information") ] in
-  let readfname fname = files := fname :: !files in
-  Arg.parse spec readfname usage;
+  let anonymous fname = files := fname :: !files in
+  Arg.parse spec anonymous usage;
   List.iter (fun s -> print_endline s) !files;
   exit 0
