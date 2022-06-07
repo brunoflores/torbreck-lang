45100edd9cd599e3f9dc92322f8dd440abbb8cc7
Bruno Flores <bruno@brunoflores.com.br>
2022-06-07 22:49:19 +1000

    Update debug event manager.

--- light/lib/event.ml
+++ light/lib/event.ml
@@ -7,6 +7,32 @@ open Modules
 
 let record_events = ref false
 
+let before env { e_loc = Loc (p1, _p2); _ } l =
+  if !record_events then
+    Levent
+      ( {
+          ev_kind = Lbefore;
+          ev_file = !defined_module.mod_name;
+          ev_char = p1;
+          ev_env = env;
+          ev_pos = 0;
+        },
+        l )
+  else l
+
+let after_pat env { p_loc = Loc (_p1, p2); _ } l =
+  if !record_events then
+    Levent
+      ( {
+          ev_kind = Lbefore;
+          ev_file = !defined_module.mod_name;
+          ev_char = p2;
+          ev_env = env;
+          ev_pos = 0;
+        },
+        l )
+  else l
+
 let after env { e_loc = Loc (_p1, p2); e_typ = ty; _ } lambda =
   if !record_events then
     Levent
