ebd0f2b8f30121925ddcb7c0c69473b229425cff
Bruno Flores <bruno@brunoflores.com.br>
2022-06-07 22:43:31 +1000

    Add prims to symbol table.

--- light/lib/symtable.ml
+++ light/lib/symtable.ml
@@ -14,14 +14,25 @@ let enter_in_numtable nt key =
   Hashtbl.add nt.num_tbl key c;
   c
 
-let custom_runtime = ref true
+let custom_runtime = ref false
 let c_prim_table = ref (new_numtable 1 : string numtable)
+
+let reserve_in_numtable nt key =
+  let _ = enter_in_numtable nt key in
+  ()
 
 let get_num_of_prim name =
   try find_in_numtable !c_prim_table name
   with Not_found ->
     if !custom_runtime then enter_in_numtable !c_prim_table name
     else begin
-      Printf.eprintf "C primitive not available: %s" name;
+      Printf.eprintf "C primitive not available: %s\n" name;
       failwith "Symtable.get_num_of_prim"
     end
+
+let reset_linker_tables () =
+  c_prim_table := new_numtable 10;
+  List.iter
+    (fun name -> reserve_in_numtable !c_prim_table name)
+    [ "print_string"; "lessthan"; "-int"; "+int" ];
+  ()
