d2954c12c4add695c81136c44e3585db0f7dbfeb
Bruno Flores <bruno@brunoflores.com.br>
2022-06-07 22:21:06 +1000

    Take input as u8 bytes.

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -35,17 +35,13 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let fname = config.filename.clone();
-  let input: Vec<i32> = if fname == "-" {
-    let buff: Vec<i32> = vec![];
-    // for b in std::io::stdin().bytes() {
-    //   buff.push(b.unwrap());
-    // }
-    buff
+  let input: Vec<u8> = if fname == "-" {
+    panic!("input from stdin not implemented");
   } else if let Ok(u8bytes) = fs::read(fname) {
-    let mut buff: Vec<i32> = vec![];
+    let mut buff: Vec<u8> = vec![];
     let mut donewithshebang = false;
-    let mut val32: i32;
-    let mut position = 0;
+    // let mut val32: i32;
+    // let mut position = 0;
     for b in u8bytes.iter() {
       let byte = *b;
       // if byte == b'#' {
@@ -57,13 +53,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
       }
       // }
 
-      val32 = (byte >> (8 * position)) as i32;
-      if position == 3 {
-        buff.push(val32);
-        position = 0;
-      } else {
-        position += 1;
-      }
+      // TODO: this will likely move to the Machine struct in some form...
+      // val32 = (byte >> (8 * position)) as i32;
+      // if position == 3 {
+      //   buff.push(val32);
+      //   position = 0;
+      // } else {
+      //   position += 1;
+      // }
+      buff.push(byte);
     }
     buff
   } else {
@@ -71,7 +69,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     process::exit(1);
   };
 
-  println!("{:?}", input);
+  // Print input bytes:
+  // println!("{:?}", input);
 
   let mut machine = Machine::new(&input[0..]);
   let accu = machine.interpret();
