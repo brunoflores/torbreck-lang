96aaeeec9a0204834ac13db0d6784f542d1e6dac
Bruno Flores <bruno@brunoflores.com.br>
2022-06-07 22:50:24 +1000

    Update lambda.

--- light/lib/lambda.ml
+++ light/lib/lambda.ml
@@ -45,6 +45,62 @@ type lambda =
   | Lvar of int
   | Lconst of struct_constant
   | Lapply of lambda * lambda list
+  | Lfunction of lambda
+  | Llet of lambda list * lambda
+  | Lletrec of (lambda * int) list * lambda
   | Lprim of primitive * lambda list
+  | Lcond of lambda * (atomic_constant * lambda) list
+  | Lswitch of int * lambda * (constr_tag * lambda) list
+  | Lstaticfail of int
+  | Lstatichandle of lambda * lambda
+  | Lhandle of lambda * lambda
+  | Lifthenelse of lambda * lambda * lambda
+  | Lsequence of lambda * lambda
+  | Lwhile of lambda * lambda
+  | Lfor of lambda * lambda * bool * lambda
+  | Lshared of lambda * int ref
   | Levent of event * lambda
 [@@deriving show]
+
+let share_lambda l = Lshared (l, ref (-1))
+
+let rec has_guard = function
+  | Lifthenelse (_, _, Lstaticfail _) -> true
+  | Levent (_, l) -> has_guard l
+  | Lshared (l, _) -> has_guard l
+  | Llet (_, l2) -> has_guard l2
+  | Lifthenelse
+      ( _,
+        _,
+        ( Lvar _ | Lconst _
+        | Lapply (_, _)
+        | Lfunction _
+        | Llet (_, _)
+        | Lletrec (_, _)
+        | Lprim (_, _)
+        | Lcond (_, _)
+        | Lswitch (_, _, _)
+        | Lstatichandle (_, _)
+        | Lhandle (_, _)
+        | Lifthenelse (_, _, _)
+        | Lsequence (_, _)
+        | Lwhile (_, _)
+        | Lfor (_, _, _, _)
+        | Lshared (_, _)
+        | Levent (_, _) ) )
+  | Lvar _ | Lconst _
+  | Lapply (_, _)
+  | Lfunction _
+  | Lletrec (_, _)
+  | Lprim (_, _)
+  | Lcond (_, _)
+  | Lswitch (_, _, _)
+  | Lstaticfail _
+  | Lstatichandle (_, _)
+  | Lhandle (_, _)
+  | Lsequence (_, _)
+  | Lwhile (_, _)
+  | Lfor (_, _, _, _) ->
+      false
+
+let guard_expression l1 l2 npops = Lifthenelse (l1, l2, Lstaticfail npops)
