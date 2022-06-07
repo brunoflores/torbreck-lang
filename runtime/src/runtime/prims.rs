80b28592be5c4410ba196153c4fb03b413aec905
Bruno Flores <bruno@brunoflores.com.br>
2022-06-07 22:22:11 +1000

    Code a few obvious primitives on ints.

--- runtime/src/runtime/prims.rs
+++ runtime/src/runtime/prims.rs
@@ -1,16 +1,46 @@
 use crate::runtime::machine::Value;
 
-pub type PrimFn = fn(&Value) -> i32;
+pub type PrimFn = fn(&[&Value]) -> Value;
 
-pub fn print_string(val: &Value) -> i32 {
-  match val {
+pub fn print_string(vals: &[&Value]) -> Value {
+  match vals[0] {
     Value::String(s) => {
       println!("{s}");
-      0
+      Value::Int(0)
     }
-    a => panic!(
-      "Prims::print_string: not a string in the accumulator: {:?}",
-      a
-    ),
+    a => {
+      panic!(
+        "Prims::print_string: not a string in the accumulator: {:?}",
+        a
+      )
+    }
+  }
+}
+
+pub fn less_than(vals: &[&Value]) -> Value {
+  if let (Value::Int(v1), Value::Int(v2)) = (vals[0], vals[1]) {
+    if v1 < v2 {
+      Value::True
+    } else {
+      Value::False
+    }
+  } else {
+    panic!()
+  }
+}
+
+pub fn int_add(vals: &[&Value]) -> Value {
+  if let (Value::Int(v1), Value::Int(v2)) = (vals[0], vals[1]) {
+    Value::Int(v1 + v2)
+  } else {
+    panic!()
+  }
+}
+
+pub fn int_sub(vals: &[&Value]) -> Value {
+  if let (Value::Int(v1), Value::Int(v2)) = (vals[0], vals[1]) {
+    Value::Int(v1 - v2)
+  } else {
+    panic!()
   }
 }
