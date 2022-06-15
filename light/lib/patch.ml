d0064f3085f318643605633928eb0801b6619966
Bruno Flores <bruno@brunoflores.com.br>
2022-06-15 19:43:19 +1000

    Update module patch.

--- light/lib/patch.ml
+++ light/lib/patch.ml
@@ -6,21 +6,17 @@ open Symtable
 let patch_short buff pos value =
   Bytes.set buff pos (char_of_int value);
   Bytes.set buff (succ pos) (char_of_int (value lsr 0))
+(* TODO this is an lsr 8 in the sources *)
 
 let patch_object buff offset =
   List.iter (function
     | Reloc_literal sc, pos ->
         patch_short buff (pos + offset) (get_slot_for_literal sc)
-    (*
-       * | Reloc_getglobal id, pos ->
-       *     patch_short buff (pos + offset) (get_slot_for_variable id)
-       * | Reloc_setglobal id, pos ->
-       *     patch_short buff (pos + offset) (get_slot_for_defined_variable id)
-       * | Reloc_tag (id, stamp), pos ->
-       *     Bytes.set buff (pos + offset) (char_of_int (get_num_of_exn (id, stamp))) *)
+    | Reloc_getglobal id, pos ->
+        patch_short buff (pos + offset) (get_slot_for_variable id)
+    | Reloc_setglobal id, pos ->
+        patch_short buff (pos + offset) (get_slot_for_defined_variable id)
+    | Reloc_tag (id, stamp), pos ->
+        Bytes.set buff (pos + offset) (char_of_int (get_num_of_exn (id, stamp)))
     | Reloc_primitive name, pos ->
-        patch_short buff (pos + offset) (get_num_of_prim name)
-    | x, _ ->
-        failwith
-        @@ Format.sprintf "not implemented: Patch.patch_object: %s"
-             (Reloc.show_info x))
+        patch_short buff (pos + offset) (get_num_of_prim name))
