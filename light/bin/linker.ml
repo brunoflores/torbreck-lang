f3d4ba817e11d8c666f6b3f4e30e611ad67ea42a
Bruno Flores <bruno@brunoflores.com.br>
2022-06-15 19:39:59 +1000

    Add -I to linker binary.

--- light/bin/linker.ml
+++ light/bin/linker.ml
@@ -1,12 +1,21 @@
 (* Linker: Command-line parsing *)
 
 open LightLib.Link
+module Misc = LightLib.Misc
 
 let () =
   let usage = "linker [-d] <file1> [<file2>] ..." in
   let debug = ref false in
+  let add_include d = Misc.load_path := d :: !Misc.load_path in
   let object_files = ref ([] : string list) in
-  let spec = [ ("-d", Arg.Set debug, "Print debug information") ] in
+  let spec =
+    [
+      ("-d", Arg.Set debug, "Print debug information");
+      ( "-I",
+        Arg.String add_include,
+        "Add directory to list of included libraries" );
+    ]
+  in
   let anonymous fname =
     let name =
       if Filename.check_suffix fname ".ml" then
