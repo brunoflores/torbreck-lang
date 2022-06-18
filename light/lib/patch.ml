cc6ca0abf0bb33b727f699a908b5f8811bdbf7c4
Bruno Flores <bruno@brunoflores.com.br>
2022-06-18 11:45:36 +1000

    Re-organise patch module.

--- light/lib/patch.ml
+++ light/lib/patch.ml
@@ -1,21 +1,22 @@
 (* Relocate a block of object bytecode *)
 
-open Reloc
 open Symtable
 
 let patch_short buff pos value =
   Bytes.set buff pos (char_of_int value);
-  Bytes.set buff (succ pos) (char_of_int (value lsr 0))
-(* TODO this is an lsr 8 in the sources *)
+  Bytes.set buff (succ pos) (char_of_int (value lsr 8))
 
-let patch_object (buff : bytes) (offset : int) : (info * int) list -> unit =
+let patch_object (buff : bytes) (offset : int) : (Reloc.info * int) list -> unit
+    =
   List.iter (function
-    | Reloc_literal sc, pos ->
+    | Reloc.Reloc_literal sc, pos ->
         patch_short buff (pos + offset) (get_slot_for_literal sc)
     | Reloc_getglobal id, pos ->
-        patch_short buff (pos + offset) (get_slot_for_variable id)
+        let v = get_slot_for_variable id in
+        patch_short buff (pos + offset) v
     | Reloc_setglobal id, pos ->
-        patch_short buff (pos + offset) (get_slot_for_defined_variable id)
+        let v = get_slot_for_defined_variable id in
+        patch_short buff (pos + offset) v
     | Reloc_tag (id, stamp), pos ->
         Bytes.set buff (pos + offset) (char_of_int (get_num_of_exn (id, stamp)))
     | Reloc_primitive name, pos ->
