60d77e4d67201d239587ed76cd9d43bcd1392812
Bruno Flores <bruno@brunoflores.com.br>
2022-06-21 07:47:05 +1000

    Turn test script into two passes.

--- light/examples/test.sh
+++ light/examples/test.sh
@@ -12,31 +12,51 @@ dune build ../bin/linker.ml
 exe_light=../../_build/default/light/bin/light.exe
 exe_linker=../../_build/default/light/bin/linker.exe
 
+# First pass: build
 for test in "$(pwd)"/*.ml; do
-    fname_out=$(basename "$test" ".ml").stdout
-    fname_exp=$(basename "$test" ".ml").stdoutexp
-    fname_exec=$(basename "$test" ".ml").out
-
     $exe_light -I ../stdlib "$test" > /dev/null
     $exe_linker -I ../stdlib exc.ml vect.ml "$test" > /dev/null
+done
 
-    ./"$fname_exec" 2> /dev/null 1> ./"$fname_out"
+# Second pass: execute test
+for test in "$(pwd)"/single_*.sh; do
+    fname_out=$(basename "$test" ".sh").stdout
+    fname_err=$(basename "$test" ".sh").stderr
+    fname_out_exp=$(basename "$test" ".sh").stdoutexp
+    fname_err_exp=$(basename "$test" ".sh").stderrexp
+
+    bash "$test" 2> ./"$fname_err" 1> ./"$fname_out"
 
     # To promote, uncomment:
-    # cp ./"$fname_out" ./"$fname_exp"
+    # cp ./"$fname_out" ./"$fname_out_exp"
+    # cp ./"$fname_err" ./"$fname_err_exp"
 
-    DIFF=$(diff ./"$fname_out" ./"$fname_exp")
+    DIFF=$(diff ./"$fname_out" ./"$fname_out_exp")
     if [ "$DIFF" != "" ]; then
         echo ""
         echo "-------------------------"
-        printf "%sFAIL%s: %s\n" "$RED" "$NOCOLOR" "$test"
+        printf "%sFAIL (stdout)%s: %s\n" "$RED" "$NOCOLOR" "$test"
         echo "$DIFF"
         echo "-------------------------"
         echo ""
 
         exit_status=1
     else
-        printf "%sPASS:%s %s\n" "$GREEN" "$NOCOLOR" "$test"
+        printf "%sPASS (stdout):%s %s\n" "$GREEN" "$NOCOLOR" "$test"
+    fi
+
+    DIFF=$(diff ./"$fname_err" ./"$fname_err_exp")
+    if [ "$DIFF" != "" ]; then
+        echo ""
+        echo "-------------------------"
+        printf "%sFAIL (stderr)%s: %s\n" "$RED" "$NOCOLOR" "$test"
+        echo "$DIFF"
+        echo "-------------------------"
+        echo ""
+
+        exit_status=1
+    else
+        printf "%sPASS (stderr):%s %s\n" "$GREEN" "$NOCOLOR" "$test"
     fi
 done
 
