ca99f5c6664fa59fe8391c5a387cd8c279ca6f42
Bruno Flores <bruno@brunoflores.com.br>
2022-06-21 09:11:06 +1000

    Add to the io interface in the stdlib.

--- light/stdlib/io.mli
+++ light/stdlib/io.mli
@@ -1,1 +1,19 @@
-value print_endline : string -> unit = 1 "print_endline";;
+(* Buffered input and output *)
+
+(* The abstract types for input channels and output channels *)
+type in_channel
+;;
+
+(* The standard input, standard output, and standard error output
+   for the process. [std_in], [std_out] and [std_err] are respectively
+   synonymous with [stdin], [stdout] and [stderr]. *)
+value std_in : in_channel
+;;
+
+(* Output functions on standard output *)
+value print_endline : string -> unit = 1 "print_endline"
+;;
+
+(* General input functions *)
+value input_char : in_channel -> char = 1 "input_char"
+;;
