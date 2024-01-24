d1c9db3c496403e891b6eaa62efd0829c0ac9d01
Bruno Flores <bruno@brunoflores.com.br>
2024-01-24 14:02:32 +1000

    Remove dir light/runtime.

--- light/runtime/lib.rs
+++
@@ -1,8 +0,0 @@
-#[derive(ocaml::IntoValue, ocaml::FromValue, Debug)]
-pub struct Atom {}
-
-#[ocaml::func]
-pub fn print_string(s: String) -> Atom {
-  println!("print from Rust: {}", s);
-  Atom {}
-}
