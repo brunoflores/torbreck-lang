a33ef1196cd00baa0d43afeb84f4db4c7fa574e6
Bruno Flores <bruno@brunoflores.com.br>
2025-05-09 04:21:06 +1000

    Organise ocamltex.

--- tools/ocamltex.ml
+++ tools/ocamltex.ml
@@ -1,16 +1,43 @@
-let () =
+(* let process_file file = () *)
+
+module Toplevel = struct
+  let ( ~! ) =
+    let memo = ref [] in
+    fun key ->
+      try List.assq key !memo
+      with Not_found ->
+        let data = Str.regexp key in
+        memo := (key, data) :: !memo;
+        data
+
+  let buffer_fmt () =
+    let b = Buffer.create 30 in
+    (b, Format.formatter_of_buffer b)
+
+  let out_fmt = buffer_fmt ()
+  let exec (_, ppf) p = ignore @@ Toploop.execute_phrase true ppf p
+
+  let parse s =
+    let lex = Lexing.from_string s in
+    let ast = Parse.toplevel_phrase lex in
+    ast
+
   let flush_fmt (b, fmt) =
     Format.pp_print_flush fmt ();
     let r = Buffer.contents b in
     Buffer.reset b;
     r
-  in
-  let buffer_fmt () =
-    let b = Buffer.create 30 in
-    (b, Format.formatter_of_buffer b)
-  in
-  let b, out_fmt = buffer_fmt () in
-  let ast = Parse.toplevel_phrase (Lexing.from_string "true;;") in
+
+  let read_output () =
+    let values =
+      Str.replace_first ~!{|^#\( *\*\)* *|} "" @@ flush_fmt out_fmt
+    in
+    values
+end
+
+let () =
   let _ = Toploop.initialize_toplevel_env () in
-  let _ = Toploop.execute_phrase true out_fmt ast in
-  print_endline @@ flush_fmt (b, out_fmt)
+  let ast = Toplevel.parse "1 + 2;;" in
+  let _ = Toplevel.(exec out_fmt) ast in
+  let out = Toplevel.read_output () in
+  print_endline out
