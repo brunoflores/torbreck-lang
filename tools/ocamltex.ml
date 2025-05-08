cfd832481f39045cae2de8b2c0fe0c7898912ff8
Bruno Flores <bruno@brunoflores.com.br>
2025-05-08 17:30:04 +1000

    Print the type.

--- tools/ocamltex.ml
+++ tools/ocamltex.ml
@@ -1,10 +1,16 @@
 let () =
+  let flush_fmt (b, fmt) =
+    Format.pp_print_flush fmt ();
+    let r = Buffer.contents b in
+    Buffer.reset b;
+    r
+  in
   let buffer_fmt () =
     let b = Buffer.create 30 in
     (b, Format.formatter_of_buffer b)
   in
-  let _, out_fmt = buffer_fmt () in
+  let b, out_fmt = buffer_fmt () in
   let ast = Parse.toplevel_phrase (Lexing.from_string "true;;") in
-  print_endline "hello";
-  Toploop.initialize_toplevel_env ();
-  Printf.printf "%b" @@ Toploop.execute_phrase true out_fmt ast
+  let _ = Toploop.initialize_toplevel_env () in
+  let _ = Toploop.execute_phrase true out_fmt ast in
+  print_endline @@ flush_fmt (b, out_fmt)
