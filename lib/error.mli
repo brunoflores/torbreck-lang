871ee7f6441d6bec6f2341001d7e158cd5694844
Bruno Flores <bruno@brunoflores.com.br>
2025-05-11 11:34:41 +1000

    Comment.

--- lib/error.mli
+++ lib/error.mli
@@ -1,17 +1,23 @@
 exception Exit of int
 
+(* An element of the type info represents a "file position":
+   file name, line number, and character position within the
+   line. Used for priting error messages. *)
 type info [@@deriving show]
 
 val dummyinfo : info
+
+(* Create file position info: file name, line number and column.  *)
 val createInfo : string -> int -> int -> int -> info
 val printInfo : ?context:info -> info -> unit
 
+(* A "value with file info". Used in the lexer and parser. *)
 type 'a withinfo = { i : info; v : 'a }
 
-(* Print an error message and fail.  The printing function is called
-     in a context where the formatter is processing an hvbox.  Insert
-     calls to Format.print_space to print a space or, if necessary,
-     break the line at that point. *)
+(* Print an error message and fail. The printing function is called
+     in a context where the formatter is processing an hvbox.
+     Insert calls to Format.print_space to print a space or,
+     if necessary, break the line at that point. *)
 val errf : (unit -> unit) -> 'a
 val errfAt : ?context:info -> info -> (unit -> unit) -> 'a
 
@@ -20,6 +26,6 @@ val errfAt : ?context:info -> info -> (unit -> unit) -> 'a
 val err : string -> 'a
 val error : ?context:info -> info -> string -> 'a
 
-(* Variants that print a message but do not fail afterwards *)
+(* Variants that print a message but do not fail afterwards. *)
 val warning : string -> unit
 val warningAt : info -> string -> unit
