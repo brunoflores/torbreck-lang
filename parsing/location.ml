d9cd2537d44fa4beb26cae4023b0b164ad7b7f55
Bruno Flores <bruno@brunoflores.com.br>
2025-05-12 05:45:11 +1000

    Update location.

--- parsing/location.ml
+++ parsing/location.ml
@@ -1,4 +1,12 @@
 open Utils
 
-type t = Warnings.loc
+type t = Warnings.loc = {
+  loc_start : Lexing.position;
+  loc_end : Lexing.position;
+}
+
+let none =
+  let loc = Lexing.dummy_pos in
+  { loc_start = loc; loc_end = loc }
+
 type 'a loc = { txt : 'a; loc : t }
--- parsing/location.mli
+++ parsing/location.mli
@@ -2,5 +2,13 @@
 
 open Utils
 
-type t = Warnings.loc
+type t = Warnings.loc = {
+  loc_start : Lexing.position;
+  loc_end : Lexing.position;
+}
+(** [t] represents a range of characters in the source code. *)
+
+val none : t
+(** An arbitrary value of type [t]. *)
+
 type 'a loc = { txt : 'a; loc : t }
