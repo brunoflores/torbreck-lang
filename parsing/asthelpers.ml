c2610072f54471600f12259618628c2f96859b54
Bruno Flores <bruno@brunoflores.com.br>
2025-05-12 05:44:52 +1000

    Update ast helpers.

--- parsing/asthelpers.ml
+++ parsing/asthelpers.ml
@@ -16,3 +16,17 @@ module Const = struct
   let char ?loc c = mk ?loc (Pconst_char c)
   let string ?(loc = !default_loc) s = mk ~loc (Pconst_string (s, loc))
 end
+
+module Exp = struct
+  let mk ?(loc = !default_loc) d =
+    { pexp_desc = d; pexp_loc = loc; pexp_loc_stack = [] }
+
+  (* let ident ?loc a = mk ?loc (Pexp_ident a) *)
+  let constant ?loc a = mk ?loc (Pexp_constant a)
+end
+
+module Str = struct
+  let mk ?(loc = !default_loc) d = { pstr_desc = d; pstr_loc = loc }
+  let eval ?loc a = mk ?loc (Pstr_eval a)
+  let value ?loc a b = mk ?loc (Pstr_value (a, b))
+end
--- parsing/asthelpers.mli
+++ parsing/asthelpers.mli
@@ -1,6 +1,6 @@
 (** Helpers to produce Parsetree fragments. *)
 
-(* open Asttypes *)
+open Asttypes
 open Parsetree
 
 type loc = Location.t
@@ -17,4 +17,17 @@ module Const : sig
   val int64 : ?loc:loc -> int64 -> constant
   val nativeint : ?loc:loc -> nativeint -> constant
   val float : ?loc:loc -> string -> constant
+end
+
+module Exp : sig
+  val mk : ?loc:loc -> expression_desc -> expression
+
+  (* val ident : ?loc:loc -> lid -> expression *)
+  val constant : ?loc:loc -> constant -> expression
+end
+
+module Str : sig
+  val mk : ?loc:loc -> structure_item_desc -> structure_item
+  val eval : ?loc:loc -> expression -> structure_item
+  val value : ?loc:loc -> rec_flag -> value_binding list -> structure_item
 end
